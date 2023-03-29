9a4cd9abd3de8f31346cb4001a2d96848a862711
王同学 <87373411+1weiyang@users.noreply.github.com>
2023-03-29 21:41:55 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,12 +1,11 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
 <script>
   var xhr = new XMLHttpRequest();
-  xhr.open('get', 'https://v1.jinrishici.com/all.json');
+  xhr.open('get', 'https://v1.jinrishici.com/all.txt');
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
-      var data = JSON.parse(xhr.responseText);
       var gushici = document.getElementById('gushici');
-      gushici.innerText = data.content;
+      gushici.innerText = xhr.responseText;
     }
   };
   xhr.send();
